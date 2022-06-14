3db465775f0fec2ad58763ecf9edef5a7ca177d1
Gayam <govinda-reddy.gayam@worldline.com>
2022-06-14 23:56:03 +0200

    Generate random IP's

--- Generate_IPv4_IPV6_MAC.py
+++ Generate_IPv4_IPV6_MAC.py
@@ -2,6 +2,7 @@
 # IPv4: 128.223.85.239, IPv6: C96A:B805:15B7:E706:8213:AFC7:8CD6:0172
 #Write a python program that asks the user to enter a URL. The program will then extract and output the following components of the URL:
 # Protocol, Domain Name, Folder Structure, File name.
+
 import random as r
 hexadecimal_digits=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
 def IPV6():
@@ -17,6 +18,41 @@ def IPV6():
     return ':'.join(final_hexa)
 ipv6_address = IPV6()
 
-    
+def mac_address():
+    mac_blocks = []
+    for hexa in range(6):
+        each_block = ''
+        for count in range(2):
+            random_integer = r.randint(0,15)
+            single_char = str(hexadecimal_digits[random_integer])
+            each_block += single_char
+        mac_blocks.append(each_block)
+    return ':'.join(mac_blocks)
+mac_address = mac_address()
+
+def ipv4():
+    ip4_list = []
+    count = 1
+    while count < 5:
+        random_number = r.randint(0,255)
+        ip4_list.append(str(random_number))
+        count += 1
+    return '.'.join(ip4_list)
+
+ipv4_address = ipv4()
+
+
+def split_url(url):
+    split_uri = url.split('/')
+    broken_url = []
+    for i in split_uri:
+        i = i.strip(':')
+        if i != '':
+            broken_url.append(i)
+    return(f"Protocol: {broken_url[0]}\nDomain Name: {broken_url[1]}\nFolder Structure: {broken_url[2]}\nFile name: {broken_url[3]}")
+
+split_url(u"https://example.com/folder/index.htm")
+
+
 
     
---
+++ SysModule.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#import sys
+#print(sys.platform) # Platform is a string identifying the OS platfrom
+#print(sys.copyright) # Copyrights of python interpreter
+#print(sys.version) # Version of Python interpretor
+# Functions in sys module
+#sys.stdout.write("Hello, This is message 1 \n")
+#sys.stderr.write("Hello, This is error message 1 \n")
+#sys.stdout and sys.stderr are almost same, but stderr output the text in red.
+#sys.getsizeof("Variable") Gets the size of variable in bytes
+#sys.argv # list of command line arguments that can be passed to python script.
+#print(sys.argv)
